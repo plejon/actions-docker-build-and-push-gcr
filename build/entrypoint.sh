b890ea9f4920210ce35997f44f74bf49befb9ff1
plejon <per.lejon@netent.com>
2020-05-27 11:25:55 +0200

    fix

---
+++ build/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+: ${GCLOUD_REGISTRY:=gcr.io}
+: ${IMAGE:=$GITHUB_REPOSITORY}
+: ${ARGS:=} # docker ARGs, ex --build-args foo=bar --build-args bruce=batman
+: ${TAG:=$GITHUB_SHA}
+: ${DEFAULT_BRANCH_TAG:=true}
+: ${LATEST:=true}
+
+docker build $ARGS -t $IMAGE:$TAG .
+docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$IMAGE:$TAG
+
+if [ $LATEST = true ]; then
+  docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$IMAGE:latest
+fi
+
+if [ "$DEFAULT_BRANCH_TAG" = "true" ]; then
+  BRANCH=$(echo $GITHUB_REF | rev | cut -f 1 -d / | rev)
+  if [ "$BRANCH" = "master" ]; then # TODO
+    docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$IMAGE:$BRANCH
+  fi
+fi
